2769935588763035a79a74294d6014994d0d881c
t0mri <thamaraiselvanr27@gmail.com>
2024-02-06 15:46:36 +0530

    add laravel.nvim

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.o.nu = true
 vim.o.rnu = true
-vim.o.tabstop = 4
+vim.o.tabstop = 2
 vim.o.swapfile = false
 vim.o.backup = false
 vim.o.udofile = true
@@ -13,17 +13,12 @@ vim.cmd("imap <C-c> <Esc>")
 local nameSpaceId = vim.api.nvim_create_namespace("markdown")
 local maxLineLength = 80
 function CheckForLongLine()
-	if (vim.fn.col('.') > maxLineLength) then
-		vim.api.nvim_buf_set_extmark(
-			0,
-			nameSpaceId,
-			vim.fn.line(".") - 1,
-			0,
-			{
-				id = vim.fn.line("."),
-				virt_text = { { "Exceeding max line length ("..maxLineLength..")", "WarningMsg" } },
-				virt_text_pos = "right_align"
-			})
+	if vim.fn.col(".") > maxLineLength then
+		vim.api.nvim_buf_set_extmark(0, nameSpaceId, vim.fn.line(".") - 1, 0, {
+			id = vim.fn.line("."),
+			virt_text = { { "Exceeding max line length (" .. maxLineLength .. ")", "WarningMsg" } },
+			virt_text_pos = "right_align",
+		})
 	else
 		vim.api.nvim_buf_del_extmark(0, nameSpaceId, vim.fn.line("."))
 	end
@@ -57,34 +52,32 @@ require("lazy").setup({
 			"rafamadriz/friendly-snippets",
 		},
 		config = function()
-			vim.api.nvim_create_autocmd('LspAttach', {
-				desc = 'LSP actions',
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
 				callback = function(event)
 					local opts = { buffer = event.buf }
-					vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-					vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-					vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-					vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-					vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-					vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-					vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-					vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-					vim.keymap.set({ 'n', 'x' }, '<F3>',
-						'<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-					vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-				end
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+					vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+					vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+					vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+					vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+					vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+					vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+					vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+					vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+				end,
 			})
 
-			local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local default_setup = function(server)
-				require('lspconfig')[server].setup({
+				require("lspconfig")[server].setup({
 					capabilities = lsp_capabilities,
 				})
 			end
 			require("mason").setup()
-			require('mason-lspconfig').setup({
-				ensure_installed = { "emmet_language_server" },
+			require("mason-lspconfig").setup({
 				handlers = {
 					default_setup,
 				},
@@ -96,15 +89,14 @@ require("lazy").setup({
 			local has_words_before = function()
 				unpack = unpack or table.unpack
 				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0 and
-				    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") ==
-				    nil
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
 
 			cmp.setup({
 				mapping = cmp.mapping.preset.insert({
 					["<C-Space>"] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
+					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
@@ -133,40 +125,40 @@ require("lazy").setup({
 				}),
 				snippet = {
 					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
+						require("luasnip").lsp_expand(args.body)
 					end,
 				},
 			})
 
 			require("luasnip.loaders.from_vscode").lazy_load()
-		end
+		end,
 	},
 	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local telescope = require("telescope")
 			telescope.setup({
 				defaults = {
-					file_ignore_patterns = { ".git", "node_modules" }
-				}
+					file_ignore_patterns = { ".git", "node_modules", "vendor" },
+				},
 			})
 
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
-		end
+		end,
 	},
 	{
-		'stevearc/oil.nvim',
+		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("oil").setup({
 				skip_confirm_for_simple_edits = true,
 			})
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>")
-		end
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -179,28 +171,30 @@ require("lazy").setup({
 					additional_vim_regex_highlighting = true,
 				},
 			})
-		end
+		end,
 	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		priority = 1000
+		priority = 1000,
 	},
 	{
-		'windwp/nvim-autopairs',
+		"windwp/nvim-autopairs",
 		event = "InsertEnter",
-		opts = {}
+		opts = {},
 	},
 	{
-		'windwp/nvim-ts-autotag',
-		opts = {}
+		"windwp/nvim-ts-autotag",
+		opts = {},
 	},
 	{
 		"folke/trouble.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-		end
+			vim.keymap.set("n", "<leader>xx", function()
+				require("trouble").toggle()
+			end)
+		end,
 	},
 	{
 		"crispgm/nvim-tabline",
@@ -230,7 +224,7 @@ require("lazy").setup({
 			local formatters = require("format-on-save.formatters")
 
 			local formatter_languages = {
-				prettierd = {
+				prettier = {
 					"css",
 					"html",
 					"json",
@@ -241,6 +235,8 @@ require("lazy").setup({
 					"typescript",
 					"typescriptreact",
 				},
+				stylua = { "lua", "luau" },
+				lsp = { "php" },
 			}
 			local formatter_by_ft = {}
 			for formatter, languages in pairs(formatter_languages) do
@@ -252,109 +248,110 @@ require("lazy").setup({
 			format_on_save.setup({
 				exclude_path_patterns = {
 					"/node_modules/",
+					"/vendor/",
 					".local/share/nvim/lazy",
 				},
-				formatter_by_ft = formatter_by_ft
+				formatter_by_ft = formatter_by_ft,
 			})
-		end
+		end,
 	},
 	{
-		'ada0l/obsidian',
+		"ada0l/obsidian",
 		keys = {
 			{
-				'<leader>ov',
+				"<leader>ov",
 				function()
 					Obsidian.select_vault()
 				end,
 				desc = "Select Obsidian vault",
 			},
 			{
-				'<leader>oo',
+				"<leader>oo",
 				function()
 					Obsidian.get_current_vault(function()
 						Obsidian.cd_vault()
 					end)
 				end,
-				desc = 'Open Obsidian directory',
+				desc = "Open Obsidian directory",
 			},
 			{
-				'<leader>ot',
+				"<leader>ot",
 				function()
 					Obsidian.get_current_vault(function()
 						Obsidian.open_today()
 					end)
 				end,
-				desc = 'Open today',
+				desc = "Open today",
 			},
 			{
-				'<leader>od',
+				"<leader>od",
 				function()
 					Obsidian.get_current_vault(function()
-						vim.ui.input({ prompt = 'Write shift in days: ' }, function(input_shift)
+						vim.ui.input({ prompt = "Write shift in days: " }, function(input_shift)
 							local shift = tonumber(input_shift) * 60 * 60 * 24
 							Obsidian.open_today(shift)
 						end)
 					end)
 				end,
-				desc = 'Open daily node with shift',
+				desc = "Open daily node with shift",
 			},
 			{
-				'<leader>on',
+				"<leader>on",
 				function()
 					Obsidian.get_current_vault(function()
-						vim.ui.input({ prompt = 'Write name of new note: ' }, function(name)
+						vim.ui.input({ prompt = "Write name of new note: " }, function(name)
 							Obsidian.new_note(name)
 						end)
 					end)
 				end,
-				desc = 'New note',
+				desc = "New note",
 			},
 			{
-				'<leader>oi',
+				"<leader>oi",
 				function()
 					Obsidian.get_current_vault(function()
-						Obsidian.select_template('telescope')
+						Obsidian.select_template("telescope")
 					end)
 				end,
-				desc = 'Insert template',
+				desc = "Insert template",
 			},
 			{
-				'<leader>os',
+				"<leader>os",
 				function()
 					Obsidian.get_current_vault(function()
-						Obsidian.search_note('telescope')
+						Obsidian.search_note("telescope")
 					end)
 				end,
-				desc = 'Search note',
+				desc = "Search note",
 			},
 			{
-				'<leader>ob',
+				"<leader>ob",
 				function()
 					Obsidian.get_current_vault(function()
-						Obsidian.select_backlinks('telescope')
+						Obsidian.select_backlinks("telescope")
 					end)
 				end,
-				desc = 'Select backlink',
+				desc = "Select backlink",
 			},
 			{
-				'<leader>og',
+				"<leader>og",
 				function()
 					Obsidian.get_current_vault(function()
 						Obsidian.go_to()
 					end)
 				end,
-				desc = 'Go to file under cursor',
+				desc = "Go to file under cursor",
 			},
 			{
-				'<leader>or',
+				"<leader>or",
 				function()
 					Obsidian.get_current_vault(function()
-						vim.ui.input({ prompt = 'Rename file to' }, function(name)
+						vim.ui.input({ prompt = "Rename file to" }, function(name)
 							Obsidian.rename(name)
 						end)
 					end)
 				end,
-				desc = 'Rename file with updating links',
+				desc = "Rename file with updating links",
 			},
 			{
 				"gf",
@@ -366,37 +363,57 @@ require("lazy").setup({
 					end
 				end,
 				noremap = false,
-				expr = true
-			}
+				expr = true,
+			},
 		},
 		opts = function()
 			return {
 				vaults = {
 					{
-						dir = '~/Notes/',
+						dir = "~/Notes/",
 						daily = {
-							dir = 'Journals/',
-							format = '%Y-%m-%d-%A',
+							dir = "Journals/",
+							format = "%Y-%m-%d-%A",
 						},
 						templates = {
-							dir = 'Templates/',
-							date = '%Y-%d-%m',
-							time = '%Y-%d-%m',
+							dir = "Templates/",
+							date = "%Y-%d-%m",
+							time = "%Y-%d-%m",
 						},
 						note = {
-							dir = '',
+							dir = "",
 							transformator = function(filename)
-								if filename ~= nil and filename ~= '' then
+								if filename ~= nil and filename ~= "" then
 									return filename
 								end
-								return string.format('%d', os.time())
+								return string.format("%d", os.time())
 							end,
 						},
-					}
-				}
+					},
+				},
 			}
-		end
+		end,
+	},
+	{
+		"adalessa/laravel.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"tpope/vim-dotenv",
+			"MunifTanjim/nui.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+		keys = {
+			{ "<leader>aa", ":Laravel artisan<cr>" },
+		},
+		event = { "VeryLazy" },
+		config = true,
 	},
 })
 
-vim.cmd("colorscheme catppuccin-latte")
+vim.cmd("colorscheme catppuccin-frappe")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeNormalFloat", { bg = "none" })
